523a4ff47730e72649bbfef151d630aaf9bf1cdc
Jai Chenchlani <jaichenchlani@gmail.com>
2020-05-13 10:19:43 -0500

    Lint formatting issue fix

--- referencestorage.py
+++ referencestorage.py
@@ -1,8 +1,8 @@
-import json
-import random
+import json, random
+from google.cloud import storage
 
 storage_client = storage.Client()
-TICKETS_BUCKET = jai_spikey_tickets_bucket
+TICKETS_BUCKET = "jai_spikey_tickets_bucket"
 
 def raise_ticket(request):
     content_type = request.headers['content-type']
@@ -29,17 +29,17 @@ def raise_ticket(request):
     tickets_dict = {}
     tickets_dict['id']=id
     tickets_dict['name']=name
-	tickets_dict['issue']=issue
+    tickets_dict['issue']=issue
 
-	bucket = storage_client.get_bucket(TICKETS_BUCKET)
-	blob = bucket.blob('parsed_tickets_data/tickets.json')
+    bucket = storage_client.get_bucket(TICKETS_BUCKET)
+    blob = bucket.blob('parsed_tickets_data/tickets.json')
     
-	if blob.exists():
+    if blob.exists():
 	    blob.download_to_filename('/tmp/tickets.json')
 
-	with open('/tmp/tickets.json','a') as fp:
-		json.dump(tickets_dict,fp,indent=2)
+    with open('/tmp/tickets.json','a') as fp:
+	    json.dump(tickets_dict,fp,indent=2)
 
-	blob.upload_from_filename('/tmp/tickets.json')
+    blob.upload_from_filename('/tmp/tickets.json')
 
-	return "Ticket generated with ID, {}".format(id)
+    return "Ticket generated with ID, {}".format(id)
